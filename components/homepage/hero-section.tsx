1ad760d4c76ebff77ad878bbc4910419c1b743a7
sheraz4196 <sheraztoor321@gmail.com>
2024-07-04 00:40:07 +0500

    feat: hero section UI

--- components/homepage/hero-section.tsx
+++ components/homepage/hero-section.tsx
@@ -18,7 +18,7 @@ export default function HeroSection() {
           duration: 0.5,
           ease: [0.4, 0.0, 0.2, 1],
         }}
-        className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+        className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-zinc-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto"
       >
         Explore our{" "}
         <Highlight className="text-black dark:text-white">
